09db72ac92737b1f08afce28c30bb8380f600b01
Doni Setiawan <d.setiawan2255@gmail.com>
2025-10-23 23:22:01 +0700

    Inisisasi Awal

---
+++ app/index.tsx
@@ -0,0 +1,21 @@
+import { Text, View, StyleSheet } from "react-native";
+
+export default function Index() {
+  return (
+    <View style = {styles.container}>
+      <Text style = {styles.text}>Hii Semuanya</Text>
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({
+  container : {
+    backgroundColor: "#25292e",
+    flex: 1,
+    justifyContent: "center",
+    alignItems: "center",
+  },
+  text :{
+    color: "white",
+  },
+});
