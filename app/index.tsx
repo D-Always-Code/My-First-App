081991407932ffe4f11e5fafa0d2b6e7d75380e0
Doni Setiawan <d.setiawan2255@gmail.com>
2025-11-02 21:14:46 +0700

    Navigasi antar Halaman

--- app/index.tsx
+++ app/index.tsx
@@ -1,9 +1,11 @@
 import { Text, View, StyleSheet } from "react-native";
+import { Link } from "expo-router";
 
 export default function Index() {
   return (
     <View style = {styles.container}>
       <Text style = {styles.text}>Hii Semuanya</Text>
+      <Link href={"/about"} style={styles.button}> Pergi ke Halaman About </Link>
     </View>
   );
 }
@@ -18,4 +20,9 @@ const styles = StyleSheet.create({
   text :{
     color: "white",
   },
+  button :{
+    color : "white",
+    fontSize: 20,
+    textDecorationLine: "underline",
+  }
 });
